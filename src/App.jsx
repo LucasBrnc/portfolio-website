150388441dab9fabc76b672de56c63c4b732c66b
Lucas Berniche <lucas.b@Lucass-MacBook-Pro.local>
2022-11-15 17:25:12 +0100

    three.js animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,27 @@
 import "./App.css";
 import React from "react";
-import Lottie from "lottie-react";
-import heroAnimation from "./assets/hero-animation.json";
+import Starpath from "./Starpath";
 
 function App() {
   return (
     <div className="App">
-
-
-      <div className="overflow-hidden">
-
-
-        <div className="h-screen">
-
-
-          <div className="relative h-3/4">
-
-
-            <Lottie
-              className="scale-150 rotate-90 translate-y-36 z-1"
-              animationData={heroAnimation}
-              loop={true}
-            ></Lottie>
-
-
-            <div className="absolute top-96 h-52 w-full bg-gradient-to-t from-black via-black/90" />
-
-
-            <div className="z-2 absolute bottom-24 px-12">
-
-
-              <div className="flex flex-col gap-3">
-                <h1 className="text-white font-mersadLight text-5xl">
-                  // QUONV
-                </h1>
-                <p className="text-white font-mersadLight text-2xl">
-                  Conversion optimisation 
-                </p>
-              </div>
-
-
-            </div>
+      <div className="h-screen overflow-hidden">
+        <div className="relative z-0">
+          <Starpath />
+          <div className="absolute z-1 bottom-0 h-2/5 w-full bg-gradient-to-t from-black/100 via-black/70" />
+          <div className="absolute bottom-8 flex flex-col justify-center items-start gap-1 h-32 w-full px-12 ">
+            <h1 className="text-white font-mersadLight text-4xl">
+              //{" "}
+              <span
+                className="text-5xl font-mersadRg text-transparent bg-clip-text
+            bg-gradient-to-br from-orange-500 to-purple-500"
+              >
+                QUONV
+              </span>
+            </h1>
+            <p className="text-white font-mersadLight text-2xl">
+              Conversion optimisation
+            </p>
           </div>
         </div>
       </div>
